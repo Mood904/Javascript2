fa8ee2a263ce14d3a2cfec5425e1ea27fd5bc5ae
Karren Kilpatrick <mood7swangs@gmail.com>
2021-10-04 13:38:45 -0400

    Video 6

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
+import Welcome from './components/Welcome'
+import React, { Component} from 'react';
 
-
-function App() {
-  return (
+class App extends Component {
+  render() {
+    return (
     <div className="App">
       <Greet />
+      <Welcome />
     </div>
   );
+ }
 }
 
 export default App;
