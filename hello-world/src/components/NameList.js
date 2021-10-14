25f5025195da50dea5f7811af8dfe8076df32c8e
Mood904 <90362029+Mood904@users.noreply.github.com>
2021-10-13 20:46:34 -0400

    Index vid 19

--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -3,41 +3,14 @@ import Person from './Person'
 
 function NameList() {
     const names= ['Karren','Otis','Ghost']
-    const persons=[
-       {
-    id: 1,
-    name: 'Karren',
-    age: 100,
-    skill: 'Invisibility'
-    },
+    const persons=[ ]
 
-    {
-
-    id: 2,
-    name: 'Otis',
-    age:  101,
-    skill:'Super Strength'
-    },
-
-    {
-    id: 3,
-    name: 'Ghost',
-    age: 102,
-    skill: 'Dying'
-}
-
-    ]
+    const nameList = names.map((name, index) => <h2 key={index}>{index} 
+    {name}</h2>)
+    return <div>{nameList}</div>
 
 
 
-
-    const personList = persons.map(person => <Person person = {person} />)
-    return (
-        <div>
-           {personList}
-            
-        </div>
-    )
 }
 
 export default NameList
